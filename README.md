22457fa35ea38acba66af9fb654f3e7dd017e838
Adam Walsh <adamwalsh14@gmail.com>
2014-02-10 17:51:49 -0500

    Inital Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<b>backupRESSettings</b>
+
+<b>What:</b> The backupRESSettings.sh script will search for RES (Reddit Ehancement Suite) installs on your computer
+and make a copy of them in an archive at a location you choose.
+
+<b>Why:</b> Backups are good, and especially right now for firefox users because firefox has been deleting res settings on reset.
+
+<b>Who:</b> This script currently only works for users running Mac OS X.  If you are running a different OS you can contact me regarding
+support and I probably will develop a build for your OS.
+
+<b>How:</b> The script works by checking the location of many different locations different browsers use to store your RES settings.  Then if
+the file is found it is added to a backup file which is stored at the location you specify.
+
+<b>Where:</b> Check the releases for the latest working version, or download the latest commit.
---
+++ backupRESSettings.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#Written on 2/10/14
+#Written by: Adam Walsh
+#This only has been tested on Mac OS X
+
+
+CHROMIUM='~/Library/Application\ Support/Chromium/Default/Local Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
+CHROME='~/Library/Application\ Support/Google/Chrome/Default/Local Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
+#SAFARI='~/Library/Safari/LocalStorage/safari-extension_com.honestbleeps.redditenhancementsuite-<10 char alphanumeric string>_0.localstorage'
+FIREFOX='~/Library/Mozilla/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg@jetpack/simple-storage/store.json'
+FIREFOX2='~/Library/Application\ Support/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json'
+
+declare -a found=()
+
+if [[ -f $CHROMIUM ]]; then
+    found += "Chromium"
+fi
+if [[ -f $CHROME ]]; then
+    found += "Chrome"
+fi
+#if [[ -f $SAFARI ]]; then
+#    found += "Safari"
+#fi
+if [[ -f $FIREFOX ]]; then
+    found += "Firefox"
+fi
+if [[ -f $FIREFOX ]]; then
+    found += "Firefox (Alternate Location)"
+fi
+
+for i in ${#found[@]}; do
+    echo "RES settings file found for $i browser"
+done
