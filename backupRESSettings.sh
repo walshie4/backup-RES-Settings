137114432b9d6274a47c3489439435d93767d037
Adam Walsh <adamwalsh14@gmail.com>
2014-02-10 18:15:01 -0500

    Fixed a typo in the firefox alternate location

--- backupRESSettings.sh
+++ backupRESSettings.sh
@@ -4,27 +4,32 @@
 #This only has been tested on Mac OS X
 
 
-CHROMIUM='~/Library/Application\ Support/Chromium/Default/Local\ Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
-CHROME='~/Library/Application\ Support/Google/Chrome/Default/Local\ Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
-#SAFARI='~/Library/Safari/LocalStorage/safari-extension_com.honestbleeps.redditenhancementsuite-<10 char alphanumeric string>_0.localstorage'
-FIREFOX='~/Library/Mozilla/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json'
-FIREFOX2='~/Library/Application\ Support/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json'
+CHROMIUM="~/Library/Application\ Support/Chromium/Default/Local\ Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage"
+CHROME="~/Library/Application\ Support/Google/Chrome/Default/Local\ Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage"
+#SAFARI="~/Library/Safari/LocalStorage/safari-extension_com.honestbleeps.redditenhancementsuite-<10 char alphanumeric string>_0.localstorage"
+FIREFOX="~/Library/Mozilla/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json"
+FIREFOX2="~/Library/Application\ Support/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json"
 
 declare -a found=()
 
 if [[ -f "$CHROMIUM" ]]; then
+    echo chromium
     found += "Chromium"
 fi
 if [[ -f "$CHROME" ]]; then
+    echo chrome
     found += "Chrome"
 fi
 #if [[ -f "$SAFARI" ]]; then
+#    echo safari
 #    found += "Safari"
 #fi
 if [[ -f "$FIREFOX" ]]; then
+    echo firefox
     found += "Firefox"
 fi
 if [[ -f "$FIREFOX2" ]]; then
+    echo firefox2
     found += "Firefox (Alternate Location)"
 fi
 
