7a33f2e4aa4bbdf7fd5a4f6f5d3b5be488c47756
Adam Walsh <adamwalsh14@gmail.com>
2014-02-10 18:07:22 -0500

    Fixed issue in chrome and chromium file locations, and firefox secondary location searching.

--- backupRESSettings.sh
+++ backupRESSettings.sh
@@ -4,27 +4,27 @@
 #This only has been tested on Mac OS X
 
 
-CHROMIUM='~/Library/Application\ Support/Chromium/Default/Local Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
-CHROME='~/Library/Application\ Support/Google/Chrome/Default/Local Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
+CHROMIUM='~/Library/Application\ Support/Chromium/Default/Local\ Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
+CHROME='~/Library/Application\ Support/Google/Chrome/Default/Local\ Storage/chrome-extension_kbmfpngjjgdllneeigpgjifpgocmfgmb_0.localstorage'
 #SAFARI='~/Library/Safari/LocalStorage/safari-extension_com.honestbleeps.redditenhancementsuite-<10 char alphanumeric string>_0.localstorage'
-FIREFOX='~/Library/Mozilla/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg@jetpack/simple-storage/store.json'
+FIREFOX='~/Library/Mozilla/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json'
 FIREFOX2='~/Library/Application\ Support/Firefox/Profiles/ohy29rj0.default/jetpack/jid1-xUfzOsOFlzSOXg\@jetpack/simple-storage/store.json'
 
 declare -a found=()
 
-if [[ -f $CHROMIUM ]]; then
+if [[ -f "$CHROMIUM" ]]; then
     found += "Chromium"
 fi
-if [[ -f $CHROME ]]; then
+if [[ -f "$CHROME" ]]; then
     found += "Chrome"
 fi
-#if [[ -f $SAFARI ]]; then
+#if [[ -f "$SAFARI" ]]; then
 #    found += "Safari"
 #fi
-if [[ -f $FIREFOX ]]; then
+if [[ -f "$FIREFOX" ]]; then
     found += "Firefox"
 fi
-if [[ -f $FIREFOX ]]; then
+if [[ -f "$FIREFOX2" ]]; then
     found += "Firefox (Alternate Location)"
 fi
 
