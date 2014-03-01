9983bd32c24782cc3975fbcadf49e3471c479bc3
Adam Walsh <adamwalsh14@gmail.com>
2014-03-01 14:53:10 -0500

    Inital commit of conversion to cross-platform version

---
+++ RESBackup.java
@@ -0,0 +1,19 @@
+/**
+ * RESBackup.java
+ *
+ * Maintained @ https://github.com/walshie4/backup-RES-Settings
+ *
+ * A simple program designed to search for installs of the
+ * Reddit Enhancement Suite on the local system and make backups.
+ * In the future it will also have the capability to restore
+ * backups to different RES installs on browsers on the local
+ * machine.
+ */
+
+public class RESBackup {
+
+    public void static main(String[] args) {a
+        //do stuff
+    }
+
+}
