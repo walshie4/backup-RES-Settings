6b87adc93a0f9f1b9f82fcde35afdb17c6093b67
Adam Walsh <adamwalsh14@gmail.com>
2014-03-01 15:57:23 -0500

    Added outline of methods

--- RESBackup.java
+++ RESBackup.java
@@ -11,9 +11,57 @@
  */
 
 public class RESBackup {
-
-    public void static main(String[] args) {a
+    private String os; /*String representation of the OS*/
+    private String browsers[]; /*Contains String representations of
+                              installed browsers on the local machine.*/
+    /**
+     * makeBackup -
+     *     Makes a backup of specified RES installs
+     */
+    public void makeBackup() {
+        //
+    }
+    /**
+     * checkForRES -
+     *     Checks for RES installs on the specified
+     *     browsers.
+     *
+     * @return An array of strings representing browsers with RES installs.
+     */
+    public String[] checkForRES() {
+        //
+    }
+    /**
+     * detectOperatingSystem -
+     *     Checks the local machine for the
+     *     which operating system it is running.
+     *
+     * @return A string representation of the
+     *         operating system on the local machine.
+     */
+    private String detectOperatingSystem() {
+        //
+    }
+    /**
+     * detectInstalledBrowsers - 
+     *     Looks for installed browsers on the
+     *     local machine.
+     *
+     * @param os - A string representation of the
+     *             detected operating system on
+     *             the local system.
+     * @return An array containing string representations
+     *         of installed browsers on the local machine.
+     */
+    private String[] detectInstalledBrowsers(String os) {
+        //
+    }
+    /**
+     * main - Runs the program
+     *
+     * @param args - Command-line arguments
+     */
+    public void static main(String[] args) {
         //do stuff
     }
-
 }
