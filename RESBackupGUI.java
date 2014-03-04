f0d186efb0a61ef32d3644d047aef489fdb77ad0
Adam Walsh <adamwalsh14@gmail.com>
2014-03-03 20:24:32 -0500

    Inital commit of View/Control

---
+++ RESBackupGUI.java
@@ -0,0 +1,36 @@
+/**
+ * RESBackupGUI.java
+ * Written by: Adam Walsh
+ * Maintained @ https://github.com/walshie4/backup-RES-Settings
+ *
+ * View and Control for RESBackup.java (Model)
+ */
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class RESBackupGUI extends JFrame implements Observer {
+    private JFrame win; /**Main window*/
+    private RESBackup model; /**Model used with this view/control*/
+    /**
+     * Constructor - Creates new RESBackupGUI object
+     *
+     * @param model Model to be used with this view/control
+     *
+     * @return new RESBackupGUI object
+     */
+    public RESBackupGUI(RESBackup model) {
+        this.model = model;
+    }
+    /**
+     * Update - Updates UI components which have had their data
+     *          changed in the model
+     *
+     * @param t - unused
+     * @param o - unused
+     *
+     */
+    public void update(Observable t, Object o) {
+        //update
+    }
+}
