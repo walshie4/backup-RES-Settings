f259ff9c2ccf628fffcb31aed6129ba86cf6c2a8
Adam Walsh <adamwalsh14@gmail.com>
2014-05-14 11:39:37 -0400

    Added inital basic layout of controller in process of splitting controller apart from inside the view file

---
+++ RESBackupController.java
@@ -0,0 +1,20 @@
+/**
+ * RESBackupController.java
+ * Written by: Adam Walsh (adw7422@rit.edu)
+ * Maintained @ https://github.com/walshie4/backup-RES-Settings
+ *
+ * Controller for the MVC design
+ */
+
+//imports here
+
+public RESBackupController {
+    public RESBackupController() {
+        //construct controller
+    }
+
+    public static void main(String[] args) {
+        //driver code
+    }
+}
+
